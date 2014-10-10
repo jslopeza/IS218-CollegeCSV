5183c8fdb75567d3ca8e92d9ce1748fa976a8e9e
jslopeza <jasonlopeza95@gmail.com>
2014-10-10 09:12:53 -0400

    Changed require to autoLoad

--- index.php
+++ index.php
@@ -10,26 +10,22 @@
 		<div class="container">
 			<?php 
 
-				require 'GetCsv.php';
-				require 'PrintCollegeName.php';
-				require 'PrintFullInfo.php';
+				require 'lib/autoload.php';
 			
 				ini_set('memory_limit', '-1');
-				//ini_set('display_errors', 1);
 				$fileName = 'hd2013.csv';
 				$parameter = 'r';
-				$csv = new GetCsv();
+				$csv = new lib\classes\GetCsv();
 				$records = $csv->getCsv($fileName, $parameter);
 
 				if(isset($_GET['college'])){
 					$index = $_GET['college'];
-					new PrintFullInfo($index, $records);
+					new lib\classes\PrintFullInfo($index, $records);
 				} else{
 					echo '<h1>Colleges in United States of America</h1>';
 					echo '<ol class="college-names">';
-					new PrintCollegeName($records);
+					new lib\classes\PrintCollegeName($records);
 					echo '</ol>';
-
 				}
 			 ?>
 		</div>
