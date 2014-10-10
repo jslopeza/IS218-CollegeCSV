2fcea8e675f2ec61d0df3edcc74b38b0f3cdd2b9
jslopeza <jasonlopeza95@gmail.com>
2014-10-10 10:39:12 -0400

    Added function to print a table either horizontal or vertical

--- lib/classes/HtmlPrint.class.php
+++ lib/classes/HtmlPrint.class.php
@@ -3,20 +3,39 @@
 	namespace lib\classes;
 	
 	class HtmlPrint{
-		static public function openTable(){
+		public function __construct($index, Array $records, $headings, $vertical = false){
 			echo '<table class="table table-bordered table-responsive">';
-		}
-
-		static public function closeTable(){
+			if($vertical == true){
+				foreach($records[$index] as $key => $value){
+					echo '<tr>';
+						echo '<th>' . $headings[$key] . '</th>';
+						echo '<td>' . $value . '</td>';
+					echo '</tr>';
+				}
+			} else {
+				echo '<thead>';
+				foreach($records[$index] as $key => $value){
+					echo '<tr>';
+						echo '<th>' . $headings[$key] . '</th>';
+					echo '</tr>';
+				}
+				echo '</thead>';
+				echo '<tbody>';
+				foreach($records[$index] as $key => $value){
+					echo '<tr>';
+						echo '<td>' . $value . '</td>';
+					echo '</tr>';
+				}
+				echo '</tbody>';
+			}
 			echo '</table>';
+
 		}
 
-		static public function openTr(){
-			echo '<tr>';
+		public function printHorizontalTable($index, Array $records){
+			
 		}
 
-		static public function closeTr(){
-			echo '</tr>';
-		}
+
 	}
  ?>
