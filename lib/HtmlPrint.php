aa341bb3822d77248489393de9a31b0b0ea9eb12
jslopeza <jasonlopeza95@gmail.com>
2014-10-10 08:38:33 -0400

    Moved file position

---
+++ lib/HtmlPrint.php
@@ -0,0 +1,20 @@
+<?php 
+
+	class HtmlPrint{
+		static public function openTable(){
+			echo '<table class="table table-bordered table-responsive">';
+		}
+
+		static public function closeTable(){
+			echo '</table>';
+		}
+
+		static public function openTr(){
+			echo '<tr>';
+		}
+
+		static public function closeTr(){
+			echo '</tr>';
+		}
+	}
+ ?>
---
+++ lib/PrintFullInfo.php
@@ -0,0 +1,33 @@
+<?php 
+
+	require 'HtmlPrint.php';
+	
+	class PrintFullInfo {
+		public function __construct($index, Array $records){
+			$headings = self::getHeadings();
+			HtmlPrint::openTable();
+			
+			foreach($records[$index] as $key => $value){
+				HtmlPrint::openTr();
+				echo '<th>' . $headings[$key] . '</th>';
+				echo '<td>' . $value . '</td>';
+				HtmlPrint::closeTr();
+			}
+			HtmlPrint::closeTable();
+		}
+
+		public function getHeadings(){
+			$fileName = 'hd2013-varlist.csv';
+			$parameter = 'r';
+			$csv = new GetCsv();
+			$headings = $csv->getCsv($fileName, $parameter);
+			$finalHeading = array();
+			foreach($headings as $heading){
+				$finalHeading[$heading['varname']] = $heading['varTitle'];
+			}
+			FileUpload::close($headings);
+			return $finalHeading;
+		}
+	}
+
+ ?>
