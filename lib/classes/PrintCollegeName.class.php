55427698b49f3e9fc5bf09117dc8da35d455bdbf
jslopeza <jasonlopeza95@gmail.com>
2014-10-13 17:28:10 -0400

    Spacing fix

--- lib/classes/PrintCollegeName.class.php
+++ lib/classes/PrintCollegeName.class.php
@@ -7,7 +7,7 @@
 			$i = -1;
 			foreach($records as $record){
 				$i++;
-				echo '<li><a target="_blank" class="college-name-link" href="?college=' .$i. '">' . $record['INSTNM'] . '</a></li>';
+				echo '<li><a class="college-name-link" href="?college=' .$i. '">' . $record['INSTNM'] . '</a></li>';
 			}
 		}
 	}
