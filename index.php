f7f48d214aa484328d09e2728c578ff4e27858c2
jslopeza <jasonlopeza95@gmail.com>
2014-10-13 17:25:10 -0400

    reference js and changed position of css file

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 		<title>College Records</title>
 		<meta charset="utf-8"/>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body>
-		<div class="container">
+		<div class="container-fluid">
+			<div class="list">
 			<?php 
 
 				require 'lib/autoload.php';
@@ -16,11 +17,12 @@
 				$fileName = 'hd2013.csv';
 				$parameter = 'r';
 				$csv = new lib\classes\GetCsv();
-				$records = $csv->getCsv($fileName, $parameter, true);
+				$records = $csv->getCsv($fileName, $parameter);
 
 				if(isset($_GET['college'])){
 					$index = $_GET['college'];
-					new lib\classes\PrintFullInfo($index, $records);
+					new lib\classes\PrintFullInfo($index, $records, true);
+
 				} else{
 					echo '<h1>Colleges in United States of America</h1>';
 					echo '<ol class="college-names">';
@@ -28,6 +30,10 @@
 					echo '</ol>';
 				}
 			 ?>
+			 </div>
+			 <div class="result"></div>
 		</div>
+		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<script type="text/javascript" src="js/ajaxPrint.js"></script>
 	</body>
 </html>
